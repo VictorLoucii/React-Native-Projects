845dd46cc7b18475043d922fcee7152628da7878
VictorLoucii <kwickslow30@gmail.com>
2025-04-23 00:13:15 +0530

    Added google sign-in using FireBase

--- FireBaseProject/src/screens/LoginScreen.tsx
+++ FireBaseProject/src/screens/LoginScreen.tsx
@@ -1,31 +1,75 @@
-import { Alert, Image, ImageBackground, Platform, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import { Alert, Button, Image, ImageBackground, Platform, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import MyButtons from '../components/MyButtons'
 import MyTextInputs from '../components/MyTextInputs'
 import SocialMedia from '../components/SocialMedia'
 import auth from '@react-native-firebase/auth'
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
-const LoginScreen = ({navigation}) => {
+
+const LoginScreen = ({ navigation }) => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    useEffect(() => {
+        console.log("GoogleSignin configured");
+        GoogleSignin.configure({
+            webClientId: '546841612840-n388461k40438kak73ts3ij3nvlf5i3u.apps.googleusercontent.com',
+        });
+    }, [])
+
+    async function onGoogleButtonPress() {
+        // Check if your device supports Google Play
+        await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+
+        // Optionally sign out first for testing account picker every time
+        await GoogleSignin.signOut();
+
+        //// Prompt user to select a Google account
+        const signInResult = await GoogleSignin.signIn();
+        console.log('SignInResult/Token:-----', signInResult)
+
+        // Try the new style of google-sign in result, from v13+ of that module
+        let idToken = signInResult.data?.idToken;
+        console.log('idToken:----', idToken);
+
+        if (!idToken) {
+            // if you are using older versions of google-signin, try old style result
+            idToken = signInResult.idToken;
+        }
+        if (!idToken) {
+            throw new Error('No ID token found');
+        }
+
+        // Create a Google credential with the token
+        const googleCredential = auth.GoogleAuthProvider.credential(signInResult.data.idToken);
+
+        Alert.alert('Sucessfully logged in');
+        
+        // Sign-in the user with the credential
+        return auth().signInWithCredential(googleCredential);
+
+    }
+
+
+
     const LoginWithEmailAndPass = () => {
         //edge case 1:
-        if(!email || !password){
+        if (!email || !password) {
             Alert.alert("All input fields need to be filled")
             return;
         }
-        auth().signInWithEmailAndPassword(email,password)
-        .then((response) => {
-            console.log('Response:------',response);
-            Alert.alert('User Logged in with email:='+email,'and password:-'+password);
-            navigation.navigate('HomeScreen');
-        })
-        .catch((error)=> {
-            console.log('Error:----',error.message);
-            Alert.alert("Error:---",error.message);
-        })
+        auth().signInWithEmailAndPassword(email, password)
+            .then((response) => {
+                console.log('Response:------', response);
+                Alert.alert('User Logged in with email:=' + email, 'and password:-' + password);
+                navigation.navigate('HomeScreen');
+            })
+            .catch((error) => {
+                console.log('Error:----', error.message);
+                Alert.alert("Error:---", error.message);
+            })
     }
 
     return (
@@ -44,27 +88,31 @@ const LoginScreen = ({navigation}) => {
                     <MyTextInputs
                         placeholder='Enter Email or username'
                         value={email}
-                        onChangeText={(text)=>{
-                            console.log('Email:----',text);
+                        onChangeText={(text) => {
+                            console.log('Email:----', text);
                             setEmail(text);
                         }}
                     />
                     <MyTextInputs
                         placeholder='Enter password'
-                        secureTextEntry = {true}
+                        secureTextEntry={true}
                         value={password}
-                        onChangeText={(text)=>{
-                            console.log("Password:------",text);
+                        onChangeText={(text) => {
+                            console.log("Password:------", text);
                             setPassword(text);
                         }}
                     />
-                    <Text style ={styles.dontHaveAccount}>Don't have an account yet?</Text>
-                    <MyButtons 
-                        title='Login' 
+                    <Text style={styles.dontHaveAccount}>Don't have an account yet?</Text>
+                    <MyButtons
+                        title='Login'
                         onpress={LoginWithEmailAndPass}
                     />
+                    <Button
+                        title={'Login with Google'}
+                        onPress={onGoogleButtonPress}
+                    />
 
-                    <Text style = {styles.textOR}>OR</Text>
+                    <Text style={styles.textOR}>OR</Text>
 
                     <SocialMedia />
 
@@ -117,14 +165,14 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
 
     },
-    dontHaveAccount:{
+    dontHaveAccount: {
         alignSelf: 'flex-end',
         marginBottom: 10,
         fontFamily: 'NovaFlat-Regular',
-        marginRight: 10,     
+        marginRight: 10,
     },
-    textOR:{
-        marginTop:20,
+    textOR: {
+        marginTop: 20,
         fontSize: 30,
         fontFamily: 'Audiowide-Regular',
     }
