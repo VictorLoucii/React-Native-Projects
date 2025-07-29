95828cf83279dd01f22cd79bcc2c52dafc8c1e5e
VictorLoucii <kwickslow30@gmail.com>
2025-07-29 16:35:34 +0530

    feat: removed react-native-file-provider and used file-viewer instead. Added app icon and splash screen. Modified UpComingEvents component and UpcomingEventsScreen. Added dynamic card rendering in HomeScreen and UpcomingEventsScreen using Supabase. Modified stling of PrayerRequestScreen and added the backend component using supabase

--- ChurchApp/ios/ShepherdCare/AppDelegate.swift
+++ ChurchApp/ios/ShepherdCare/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "ChurchApp",
+      withModuleName: "Shepherd Care",
       in: window,
       launchOptions: launchOptions
     )
